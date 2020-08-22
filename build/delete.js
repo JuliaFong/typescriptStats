f0929b566956dd5997ec64f42a15bae75abfd1fe
Julia Fong <Juliamzfong@gmail.com>
2020-08-22 16:28:32 -0700

    Refactor abstract and generic classes

---
+++ build/delete.js
@@ -0,0 +1,14 @@
+"use strict";
+//NOTHING TO DO WITH GENERICS
+var add = function (a, b) {
+    return a + b;
+};
+add(20, 572);
+add(3897, 2746987346);
+var HoldNumber = /** @class */ (function () {
+    function HoldNumber() {
+    }
+    return HoldNumber;
+}());
+var holdNumber = new HoldNumber();
+holdNumber.data = 123;
