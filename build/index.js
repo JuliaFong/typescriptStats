1c521b2a27da4bdcfe1114165a2d51649fe1ab68
Julia Fong <Juliamzfong@gmail.com>
2020-08-21 19:53:59 -0700

    Debug index.ts file

--- build/index.js
+++ build/index.js
@@ -1,2 +1,11 @@
 "use strict";
-console.log('hello world');
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+var fs_1 = __importDefault(require("fs"));
+//fs stands for file system
+var matches = fs_1.default.readFileSync('football.csv', {
+    encoding: "utf-8"
+});
+console.log(matches);
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,8 @@
-console.log('hello world');
+import fs from 'fs';
+//fs stands for file system
+
+const matches = fs.readFileSync('football.csv', {
+    encoding: "utf-8"
+});
+
+console.log(matches)
