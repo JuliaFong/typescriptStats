fce04d24e1c52e7763b6b917950a4f9c1af4b7ad
Julia Fong <Juliamzfong@gmail.com>
2020-08-21 19:58:43 -0700

    Update index.ts file

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,14 @@
 import fs from 'fs';
 //fs stands for file system
 
-const matches = fs.readFileSync('football.csv', {
+const matches = fs
+.readFileSync('football.csv', {
     encoding: "utf-8"
+})
+.split("/n")
+.map((row: string): string[] => {
+    return row.split(',')
 });
+
 
 console.log(matches)
